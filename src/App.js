3ed27c014a2f9f2741a9e8a6759d97dcd24d355a
Zack Hatlen <zckhtln@icloud.com>
2022-08-17 13:02:23 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import { fetchUtils, Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import woocommerceData from './ra-data-woocommerce';
 
-const woocommerceUrl = 'https://rays.com.tr/wp-json/wc/v3';
-const consumerKey = 'ck_4b7c9c94dd61b458e7a0a13e40c8f1b98a395310';
-const consumerSecret = 'cs_de8d232a90f55d73806e71c79e29b9fc079111da';
-
-const httpClient = (url, options = {}) => {
-    if (!options.headers) {
-        options.headers = new Headers({ Accept: 'application/json' });
-    }
-    const token = `${consumerKey}:${consumerSecret}`
-    const basic_auth = btoa(unescape(encodeURIComponent(token)))
-    options.user = {
-        authenticated: true,
-        token: 'Basic ' + basic_auth
-    };
-    return fetchUtils.fetchJson(url, options);
-};
-
-const dataProvider = woocommerceData(woocommerceUrl, httpClient);
+const dataProvider = woocommerceData({
+    woocommerceUrl: 'https://rays.com.tr/wp-json/wc/v3',
+    consumerKey: 'ck_4b7c9c94dd61b458e7a0a13e40c8f1b98a395310',
+    consumerSecret: 'cs_de8d232a90f55d73806e71c79e29b9fc079111da',
+})
 
 const App = () => (
     <Admin dataProvider={dataProvider}>
